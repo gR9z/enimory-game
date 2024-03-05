bc72d611324b002838b2de29c7a38e20a979308b
Royer Gaétan <royergaetan90@gmail.com>
2024-03-05 01:46:11 +0100

    add debounce for optimization on adjust-nav-top

--- js/common/adjust-nav-top.js
+++ js/common/adjust-nav-top.js
@@ -1,13 +1,14 @@
+import debounce from '../utils/debounce.js';
+
 const adjustNavTop = () => {
     const header = document.querySelector('.header');
     const headerHeight = header.offsetHeight;
-
     document.documentElement.style.setProperty(
         '--header-height',
         `${headerHeight}px`
     );
 };
 
-window.addEventListener('resize', adjustNavTop);
+window.addEventListener('resize', debounce(adjustNavTop, 300));
 
 export default adjustNavTop;
---
+++ js/utils/debounce.js
@@ -0,0 +1,13 @@
+const debounce = (fn, delay) => {
+    let id;
+
+    return (...args) => {
+        if (id) clearTimeout(id);
+
+        id = setTimeout(() => {
+            fn(...args);
+        }, delay);
+    };
+};
+
+export default debounce;
